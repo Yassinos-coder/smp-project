2e0c86831182403c9502e8325fcce79272320c46
Yassinos-Coder <castroyassine0@gmail.com>
2022-12-29 00:42:51 +0100

    All Good

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import Store from './redux/store';
-
+// Coded By Yassinos-Coder
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
